5e1d20d8dbc9bd7f2938cc8e2f01be91de5d85bf
andrachiorcea <achiorcea27@gmail.com>
2019-03-16 07:43:56 +0200

    commit bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,30 @@
 #/bin/bash
-echo "Am ajuns aici"
+
+#update programs
+
+if [[`yum update`]]; then
+  echo "updated successful" >&2
+else
+  ret=$?
+  echo "command failed with exit code $ret" >&2
+fi
+
+#interface configuration
+
+for intf in /sys/class/net/*; do
+    echo "updating interface $intf"
+    sudo ifconfig `basename $intf` up
+done
+
+#disabling password authentication
+
+selinux_status='getenforce'
+if ["$selinux_status" == "disabled"]; then
+    echo "Selinux is disabled"
+    if [[`setenforce 0`]]; then 
+        echo "Setenforce command executed successfully"
+    else
+        ret=$?
+        "command setenforce failed with exit code $ret" 
+    fi
+
